657dcb89d5d2fd8303cb919d26a8da309a524cbd
Calaway <calaway@users.noreply.github.com>
2017-03-06 21:21:00 -0700

    Refactor logic out of sessions controller to user model.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,17 +1,15 @@
 class SessionsController < ApplicationController
   def new
-    @facebook = {client_id: ENV['FACEBOOK_KEY'],
-                 redirect_uri: "http://localhost:3001/auth/facebook/callback"}
   end
 
   def create
-    user = User.find_or_create_by(uid: request.env['omniauth.auth'][:uid]) do |user|
-      user.provider = request.env['omniauth.auth'][:provider]
-      user.name = request.env['omniauth.auth'][:info][:name]
-      user.oauth_token = request.env['omniauth.auth'][:credentials][:token]
-      user.oauth_token_expires_at = Time.at(request.env['omniauth.auth'][:credentials][:expires_at])
-    end
+    user = User.find_or_create_from_oauth(oauth_info)
     session[:user_id] = user.id
     redirect_to "/"
+  end
+
+private
+  def oauth_info
+    request.env['omniauth.auth']
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+  def self.find_or_create_from_oauth(oauth_info)
+    find_or_create_by(uid: oauth_info[:uid]) do |user|
+      user.provider = oauth_info[:provider]
+      user.name = oauth_info[:info][:name]
+      user.oauth_token = oauth_info[:credentials][:token]
+      user.oauth_token_expires_at = Time.at(oauth_info[:credentials][:expires_at])
+    end
+  end
 end
