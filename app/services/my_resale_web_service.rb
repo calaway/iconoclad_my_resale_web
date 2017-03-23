792eeaf9157968d6b6cb845023621ee8ca01a3ac
Calaway <calaway@users.noreply.github.com>
2017-03-23 17:04:47 -0600

    Refactored my resale web service for single responsibility principle.

--- app/services/my_resale_web_service.rb
+++ app/services/my_resale_web_service.rb
@@ -1,24 +1,41 @@
 module MyResaleWebService
-  def self.fetch_balance(consignor_id, consignor_last_name)
-    agent = Mechanize.new
-    signin_page = agent.get('http://www.myresaleweb.com/')
+  def self.balance_page_content(consignor_id, consignor_last_name)
+    balance_page = signin(consignor_id, consignor_last_name)
+    parse_balance_page_content(balance_page)
+  end
+
+  def self.signin(consignor_id, consignor_last_name)
+    signin_page = open_web_page('http://www.myresaleweb.com/')
     signin_form = signin_page.form_with(action: "../signin.asp")
+    form_values = {"state" => "UT",
+                   "storeid" => "BK1652",
+                   "consignorid" => consignor_id,
+                   "consignorlastname" => consignor_last_name}
+    fill_form(signin_form, form_values)
+    submit_with_button(signin_form, "Check My Account")
+  end
 
-    signin_form.state = "UT"
-    signin_form.storeid = "BK1652"
-    signin_form.consignorid = consignor_id
-    signin_form.consignorlastname = consignor_last_name
+  def self.open_web_page(url)
+    agent = Mechanize.new
+    agent.get(url)
+  end
 
-    submit_button = signin_form.button_with(value: "Check My Account")
-    balances_page = signin_form.click_button(submit_button)
-    balances_content_raw = balances_page.css("div#consigner_content_text p")
+  def self.fill_form(form, values)
+    values.each do |key, value|
+      form[key] = value
+    end
+  end
+
+  def self.submit_with_button(form, button_value)
+    submit_button = form.button_with(value: button_value)
+    form.click_button(submit_button)
+  end
+
+  def self.parse_balance_page_content(balance_page)
+    balances_content_raw = balance_page.css("div#consigner_content_text p")
     balances_content = balances_content_raw.map do |p_tag|
-      [
-        p_tag.at_css("strong").text.chomp(":"),
-        p_tag.at_css("span").text
-      ]
+      [p_tag.at_css("strong").text.chomp(":"),
+       p_tag.at_css("span").text]
     end.to_h
-
-    balance = balances_content["Balance"]
   end
 end
--- spec/services/my_resale_web_service_spec.rb
+++ spec/services/my_resale_web_service_spec.rb
@@ -1,7 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe MyResaleWebService, type: :service do
-  it "fetches the consignor's balance" do
-    expect(MyResaleWebService.fetch_balance(ENV["MY_RESALE_WEB_ID_1"], ENV["MY_RESALE_WEB_NAME_1"])).to eq "$33.411"
+  it "fetches the balance screen data" do
+    id   = ENV["MY_RESALE_WEB_ID_1"]
+    name = ENV["MY_RESALE_WEB_NAME_1"]
+    balance_page_content = MyResaleWebService.balance_page_content(id, name)
+
+    # Expect balance to be of the format "$1234.56"
+    expect(balance_page_content["Balance"]).to match(/\A\$\d+\.\d{2}\z/)
+    expect(balance_page_content["Name"]).to eq("Jodi Mardesich-Smith")
   end
 end
