f964b3ba6f42d05d954cdc13889bc3234d0c8680
Calaway <calaway@users.noreply.github.com>
2017-06-07 23:42:24 -0600

    Address rubocop style guide offences.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-private
+  private
+
   before_action :authorize!
   helper_method :current_user
 
@@ -10,11 +11,10 @@ private
   end
 
   def authorize!
-    is_login_path = params[:controller] == "sessions" &&
-                    params[:action].in?(%w(new create failure))
-    unless current_user || is_login_path
-      puts "🔥 Not authorized to access #{params[:controller]}##{params[:action]} - Redirecting🔥"
-      redirect_to login_path
-    end
+    is_login_path = params[:controller] == 'sessions' &&
+                    params[:action].in?(%w[new create failure])
+    return if current_user || is_login_path
+    puts "🔥 Not authorized to access #{params[:controller]}##{params[:action]} - Redirecting🔥"
+    redirect_to login_path
   end
 end
