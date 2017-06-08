3adf8e0bfaa3648d7b8512b1c3ebe8bee7c2529e
Calaway <calaway@users.noreply.github.com>
2017-06-07 23:36:26 -0600

    Address rubocop style guide offences.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,23 +1,23 @@
 class SessionsController < ApplicationController
-  def new
-  end
+  def new; end
 
   def create
     user = User.find_or_create_from_oauth(oauth_info)
     session[:user_id] = user.id
-    redirect_to "/", notice: "Login Successful"
+    redirect_to '/', notice: 'Login Successful'
   end
 
   def failure
-    redirect_to login_path, notice: "Login Unsuccessful"
+    redirect_to login_path, notice: 'Login Unsuccessful'
   end
 
   def destroy
     reset_session
-    redirect_to login_path, notice: "Logout Successful"
+    redirect_to login_path, notice: 'Logout Successful'
   end
 
-private
+  private
+
   def oauth_info
     request.env['omniauth.auth']
   end
